ed9a7d6a5c4f183e0005bb0e36ad3b22178e75e0
200466543 <77888976+200466543@users.noreply.github.com>
2021-03-30 18:21:40 -0400

    setting up packages (properly now)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
     public static void main(String[] args) {
-        System.out.println("Testing Main Class");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("views/cardView.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("MTG Card Creator");
+        stage.show();
     }
 }
